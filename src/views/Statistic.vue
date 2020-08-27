14d222dd64c43702043bc575031e82c0fe00d6ed
nurf21 <nurfauzangiri@gmail.com>
2020-08-27 21:01:23 +0700

    :card_file_box: Fetch data from database

--- src/views/Statistic.vue
+++ src/views/Statistic.vue
@@ -16,28 +16,14 @@
                 <h3>
                   Rp.
                   {{
-                    todayIncome.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
+                  todayIncome.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
                   }}
                 </h3>
-                <p v-if="todayIncome > prevIncome">
-                  +{{ dailyProfit() }}% Yesterday
-                </p>
-                <p v-else>{{ dailyProfit() }}% Yesterday</p>
-                <img
-                  src="@/assets/img/Ellipse 1.png"
-                  alt="ellipse"
-                  class="ellipse1"
-                />
-                <img
-                  src="@/assets/img/Ellipse 1.png"
-                  alt="ellipse"
-                  class="ellipse2"
-                />
-                <img
-                  src="@/assets/img/Ellipse 1.png"
-                  alt="ellipse"
-                  class="ellipse3"
-                />
+                <p v-if="todayIncome > prevIncome">+{{ profitGrowth() }}% Yesterday</p>
+                <p v-else-if="todayIncome < prevIncome">{{ profitGrowth() }}% Yesterday</p>
+                <img src="@/assets/img/Ellipse 1.png" alt="ellipse" class="ellipse1" />
+                <img src="@/assets/img/Ellipse 1.png" alt="ellipse" class="ellipse2" />
+                <img src="@/assets/img/Ellipse 1.png" alt="ellipse" class="ellipse3" />
               </div>
             </article>
           </b-col>
@@ -46,23 +32,12 @@
             <article class="order">
               <div class="article-container">
                 <p>Orders</p>
-                <h3>{{ totalData }}</h3>
-                <p>+5% Last Week</p>
-                <img
-                  src="@/assets/img/Ellipse 2.png"
-                  alt="ellipse"
-                  class="ellipse1"
-                />
-                <img
-                  src="@/assets/img/Ellipse 2.png"
-                  alt="ellipse"
-                  class="ellipse2"
-                />
-                <img
-                  src="@/assets/img/Ellipse 2.png"
-                  alt="ellipse"
-                  class="ellipse3"
-                />
+                <h3>{{ countThisWeek }}</h3>
+                <p v-if="countThisWeek > countPrevWeek">+{{ orderGrowth() }}% Last Week</p>
+                <p v-else-if="countThisWeek < countPrevWeek">{{ orderGrowth() }}% Last Week</p>
+                <img src="@/assets/img/Ellipse 2.png" alt="ellipse" class="ellipse1" />
+                <img src="@/assets/img/Ellipse 2.png" alt="ellipse" class="ellipse2" />
+                <img src="@/assets/img/Ellipse 2.png" alt="ellipse" class="ellipse3" />
               </div>
             </article>
           </b-col>
@@ -71,23 +46,12 @@
             <article class="year-income">
               <div class="article-container">
                 <p>This Year's Income</p>
-                <h3>Rp. 100.000.000.000</h3>
-                <p>+10% Last Year</p>
-                <img
-                  src="@/assets/img/Ellipse 3.png"
-                  alt="ellipse"
-                  class="ellipse1"
-                />
-                <img
-                  src="@/assets/img/Ellipse 3.png"
-                  alt="ellipse"
-                  class="ellipse2"
-                />
-                <img
-                  src="@/assets/img/Ellipse 3.png"
-                  alt="ellipse"
-                  class="ellipse3"
-                />
+                <h3>Rp. {{yearIncome.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')}}</h3>
+                <p v-if="yearIncome > prevYearIncome">+{{ yearGrowth() }}% Last Year</p>
+                <p v-else-if="yearIncome < prevYearIncome">{{ yearGrowth() }}% Last Year</p>
+                <img src="@/assets/img/Ellipse 3.png" alt="ellipse" class="ellipse1" />
+                <img src="@/assets/img/Ellipse 3.png" alt="ellipse" class="ellipse2" />
+                <img src="@/assets/img/Ellipse 3.png" alt="ellipse" class="ellipse3" />
               </div>
             </article>
           </b-col>
@@ -97,14 +61,23 @@
             <p style="font-size: 30px;">Revenue</p>
           </b-col>
           <b-col cols="6" style="text-align: end; margin-top: 20px">
-            <b-dropdown id="dropdown-1" text="Month" class="m-md-2">
-              <b-dropdown-item v-for="(value, index) in month" :key="index">{{
-                value
-              }}</b-dropdown-item>
+            <b-dropdown id="dropdown-1" :text="month" class="m-md-2">
+              <b-dropdown-item @click="chartJan()">January</b-dropdown-item>
+              <b-dropdown-item @click="chartFeb()">February</b-dropdown-item>
+              <b-dropdown-item @click="chartMar()">March</b-dropdown-item>
+              <b-dropdown-item @click="chartApr()">April</b-dropdown-item>
+              <b-dropdown-item @click="chartMay()">May</b-dropdown-item>
+              <b-dropdown-item @click="chartJun()">June</b-dropdown-item>
+              <b-dropdown-item @click="chartJul()">July</b-dropdown-item>
+              <b-dropdown-item @click="chartAug()">August</b-dropdown-item>
+              <b-dropdown-item @click="chartSep()">September</b-dropdown-item>
+              <b-dropdown-item @click="chartOct()">October</b-dropdown-item>
+              <b-dropdown-item @click="chartNov()">November</b-dropdown-item>
+              <b-dropdown-item @click="chartDec()">December</b-dropdown-item>
             </b-dropdown>
           </b-col>
           <div class="chart-container">
-            <line-chart :data="chartData" legend="bottom" />
+            <line-chart :data="chartData" />
           </div>
         </b-row>
 
@@ -113,31 +86,20 @@
             <p style="font-size: 30px;">Recent Order</p>
           </b-col>
           <b-col cols="6" style="text-align: end; margin-top: 20px">
-            <b-dropdown id="dropdown-1" text="Today" class="m-md-2">
-              <b-dropdown-item>Today</b-dropdown-item>
-              <b-dropdown-item>This Week</b-dropdown-item>
-              <b-dropdown-item>This Month</b-dropdown-item>
+            <b-dropdown id="dropdown-1" :text="text" class="m-md-2">
+              <b-dropdown-item @click="getHistoryToday()">Today</b-dropdown-item>
+              <b-dropdown-item @click="getHistoryWeek()">This Week</b-dropdown-item>
+              <b-dropdown-item @click="getHistoryMonth()">This Month</b-dropdown-item>
             </b-dropdown>
           </b-col>
           <div style="width: 100%">
-            <b-table
-              striped
-              hover
-              :items="items"
-              style="text-align: center"
-            ></b-table>
+            <b-table striped hover :items="items" style="text-align: center"></b-table>
           </div>
         </b-row>
       </b-col>
     </b-row>
 
-    <b-sidebar
-      id="sidebar-backdrop"
-      :title="msg"
-      backdrop-variant="dark"
-      backdrop
-      shadow
-    >
+    <b-sidebar id="sidebar-backdrop" :title="msg" backdrop-variant="dark" backdrop shadow>
       <Sidebar />
     </b-sidebar>
   </b-container>
@@ -158,64 +120,114 @@ export default {
     return {
       greet: 'Welcome',
       user: 'Cashier #1',
-      month: [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October',
-        'November',
-        'December'
-      ],
-      items: [
-        {
-          INVOICES: '#10928',
-          CASHIER: 'Cashier 1',
-          DATE: '06 October 2019',
-          ORDERS: 'Ice Tea, Salad With peanut sauce',
-          AMOUNT: 'Rp. 120.000'
-        }
-      ],
-      chartData: [
-        {
-          name: 'This Month',
-          data: {
-            '2020-08-21': Math.floor(Math.random() * 100000 + 1),
-            '2020-08-22': Math.floor(Math.random() * 100000 + 1)
-          }
-        },
-        {
-          name: 'Last Month',
-          data: {
-            '2020-08-21': Math.floor(Math.random() * 100000 + 1),
-            '2020-08-22': Math.floor(Math.random() * 100000 + 1)
-          }
-        }
-      ],
+      month: 'Month',
+      text: 'Today',
+      items: [],
+      chartData: [],
       currDate: new Date().toJSON().slice(0, 10),
-      todayIncome: 0,
       prevDate: new Date(Date.now() - 864e5).toJSON().slice(0, 10),
+      prevWeek: new Date(new Date().getFullYear(), new Date().getMonth(),
+        new Date().getDate() - 7).toJSON().slice(0, 10),
+      prevYear: new Date(new Date().getFullYear() - 1, new Date().getMonth(),
+        new Date().getDate()).toJSON().slice(0, 10),
+      todayIncome: 0,
       prevIncome: 0,
-      totalData: 0,
+      countThisWeek: 0,
+      countPrevWeek: 0,
+      yearIncome: 0,
+      prevYearIncome: 0,
       history: []
     }
   },
   created() {
-    this.getHistory()
+    this.getHistoryToday()
+    this.getDataChart()
     this.getTodayIncome()
     this.getPrevIncome()
+    this.getYearIncome()
+    this.getPrevYearIncome()
+    this.getCountHistoryWeek()
+    this.getCountHistoryLastWeek()
   },
   methods: {
-    getHistory() {
-      axios.get('http://127.0.0.1:3001/history?limit=100').then(response => {
-        this.history = response.data.data
-        this.totalData = response.data.pagination.totalData
-      })
+    getHistoryToday() {
+      this.items = []
+      axios
+        .get('http://127.0.0.1:3001/history/today')
+        .then(response => {
+          this.history = response.data.data
+          this.history.map((value) => {
+            const setItem = {
+              INVOICES: `#${value.history_invoice}`,
+              CASHIER: this.user,
+              DATE: value.history_created_at.slice(0, 10),
+              ORDERS: value.orders.map(item => item.product_name).join(', '),
+              AMOUNT: `Rp. ${value.history_subtotal.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')}`
+            }
+            this.items = [...this.items, setItem]
+          })
+          this.text = 'Today'
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    getHistoryWeek() {
+      this.items = []
+      axios
+        .get('http://127.0.0.1:3001/history/week')
+        .then(response => {
+          this.history = response.data.data
+          this.history.map((value) => {
+            const setItem = {
+              INVOICES: `#${value.history_invoice}`,
+              CASHIER: this.user,
+              DATE: value.history_created_at.slice(0, 10),
+              ORDERS: value.orders.map(item => item.product_name).join(', '),
+              AMOUNT: `Rp. ${value.history_subtotal.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')}`
+            }
+            this.items = [...this.items, setItem]
+          })
+          this.text = 'This Week'
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    getHistoryMonth() {
+      this.items = []
+      axios
+        .get('http://127.0.0.1:3001/history/month')
+        .then(response => {
+          this.history = response.data.data
+          this.history.map((value) => {
+            const setItem = {
+              INVOICES: `#${value.history_invoice}`,
+              CASHIER: this.user,
+              DATE: value.history_created_at.slice(0, 10),
+              ORDERS: value.orders.map(item => item.product_name).join(', '),
+              AMOUNT: `Rp. ${value.history_subtotal.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')}`
+            }
+            this.items = [...this.items, setItem]
+          })
+          this.text = 'This Month'
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    getDataChart() {
+      axios
+        .get(`http://127.0.0.1:3001/history/chart?date=${this.currDate}`)
+        .then(response => {
+          const setChart = response.data.data
+          for (let i = 0; i < setChart.length; i++) {
+            this.chartData.push([setChart[i].date, setChart[i].sum])
+          }
+        })
+        .catch(error => {
+          console.log(error)
+        })
     },
     getTodayIncome() {
       axios
@@ -237,10 +249,142 @@ export default {
           console.log(error)
         })
     },
-    dailyProfit() {
+    getYearIncome() {
+      axios
+        .get(`http://127.0.0.1:3001/history/incomeyear?date=${this.currDate}`)
+        .then(response => {
+          this.yearIncome = response.data.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    getPrevYearIncome() {
+      axios
+        .get(`http://127.0.0.1:3001/history/incomeyear?date=${this.prevYear}`)
+        .then(response => {
+          this.prevYearIncome = response.data.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    getCountHistoryWeek() {
+      axios
+        .get(`http://127.0.0.1:3001/history/count?date=${this.currDate}`)
+        .then(response => {
+          this.countThisWeek = response.data.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    getCountHistoryLastWeek() {
+      axios
+        .get(`http://127.0.0.1:3001/history/count?date=${this.prevWeek}`)
+        .then(response => {
+          this.countPrevWeek = response.data.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    profitGrowth() {
       const count =
         ((this.todayIncome - this.prevIncome) / this.prevIncome) * 100
       return Math.ceil(count)
+    },
+    orderGrowth() {
+      const count = ((this.countThisWeek - this.countPrevWeek) / this.countPrevWeek) * 100
+      return Math.ceil(count)
+    },
+    yearGrowth() {
+      const count = ((this.yearIncome - this.prevYearIncome) / this.prevYearIncome) * 100
+      return Math.ceil(count)
+    },
+    chartJan() {
+      this.month = 'January'
+      this.chartData = []
+      this.currDate = new Date(new Date().getFullYear(), 0, 2).toJSON().slice(0, 10)
+      console.log(this.currDate)
+      this.getDataChart()
+    },
+    chartFeb() {
+      this.month = 'February'
+      this.chartData = []
+      this.currDate = new Date(new Date().getFullYear(), 1, 2).toJSON().slice(0, 10)
+      console.log(this.currDate)
+      this.getDataChart()
+    },
+    chartMar() {
+      this.month = 'March'
+      this.chartData = []
+      this.currDate = new Date(new Date().getFullYear(), 2, 2).toJSON().slice(0, 10)
+      console.log(this.currDate)
+      this.getDataChart()
+    },
+    chartApr() {
+      this.month = 'April'
+      this.chartData = []
+      this.currDate = new Date(new Date().getFullYear(), 3, 2).toJSON().slice(0, 10)
+      console.log(this.currDate)
+      this.getDataChart()
+    },
+    chartMay() {
+      this.month = 'May'
+      this.chartData = []
+      this.currDate = new Date(new Date().getFullYear(), 4, 2).toJSON().slice(0, 10)
+      console.log(this.currDate)
+      this.getDataChart()
+    },
+    chartJun() {
+      this.month = 'June'
+      this.chartData = []
+      this.currDate = new Date(new Date().getFullYear(), 5, 2).toJSON().slice(0, 10)
+      console.log(this.currDate)
+      this.getDataChart()
+    },
+    chartJul() {
+      this.month = 'July'
+      this.chartData = []
+      this.currDate = new Date(new Date().getFullYear(), 6, 2).toJSON().slice(0, 10)
+      console.log(this.currDate)
+      this.getDataChart()
+    },
+    chartAug() {
+      this.month = 'August'
+      this.chartData = []
+      this.currDate = new Date(new Date().getFullYear(), 7, 2).toJSON().slice(0, 10)
+      console.log(this.currDate)
+      this.getDataChart()
+    },
+    chartSep() {
+      this.month = 'September'
+      this.chartData = []
+      this.currDate = new Date(new Date().getFullYear(), 8, 2).toJSON().slice(0, 10)
+      console.log(this.currDate)
+      this.getDataChart()
+    },
+    chartOct() {
+      this.month = 'October'
+      this.chartData = []
+      this.currDate = new Date(new Date().getFullYear(), 9, 2).toJSON().slice(0, 10)
+      console.log(this.currDate)
+      this.getDataChart()
+    },
+    chartNov() {
+      this.month = 'November'
+      this.chartData = []
+      this.currDate = new Date(new Date().getFullYear(), 10, 2).toJSON().slice(0, 10)
+      console.log(this.currDate)
+      this.getDataChart()
+    },
+    chartDec() {
+      this.month = 'December'
+      this.chartData = []
+      this.currDate = new Date(new Date().getFullYear(), 11, 2).toJSON().slice(0, 10)
+      console.log(this.currDate)
+      this.getDataChart()
     }
   },
   computed: {
