8e4a4f9c4a1b7ad7de99275230c1a18b5d2c0277
nurf21 <nurfauzangiri@gmail.com>
2020-08-31 15:17:50 +0700

    :zap: SPA impelementation

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,25 +1,43 @@
 <template>
   <b-container fluid>
     <b-img :src="avatar" class="avatar" fluid />
-    <h5 class="user">{{user}}</h5>
+    <h5 class="user">{{ user }}</h5>
     <hr />
     <b-navbar variant="faded" type="light">
-      <b-navbar-brand href="/">
-        <img src="@/assets/img/waiter.png" class="mr-2 d-inline-block align-top" alt="foods" />
-        Food and Drink
-      </b-navbar-brand>
+      <router-link to="/">
+        <b-navbar-brand>
+          <img
+            src="@/assets/img/waiter.png"
+            class="mr-2 d-inline-block align-top"
+            alt="foods"
+          />
+          Food and Drink
+        </b-navbar-brand>
+      </router-link>
     </b-navbar>
     <b-navbar variant="faded" type="light">
-      <b-navbar-brand href="statistic">
-        <img src="@/assets/img/dashboard.png" class="mr-2 d-inline-block align-top" alt="statistic" />
-        Statistic
-      </b-navbar-brand>
+      <router-link to="/statistic">
+        <b-navbar-brand>
+          <img
+            src="@/assets/img/dashboard.png"
+            class="mr-2 d-inline-block align-top"
+            alt="statistic"
+          />
+          Statistic
+        </b-navbar-brand>
+      </router-link>
     </b-navbar>
     <b-navbar variant="faded" type="light">
-      <b-navbar-brand href="setting">
-        <img src="@/assets/img/settings.png" class="mr-2 d-inline-block align-top" alt="setting" />
-        Setting
-      </b-navbar-brand>
+      <router-link to="/setting">
+        <b-navbar-brand>
+          <img
+            src="@/assets/img/settings.png"
+            class="mr-2 d-inline-block align-top"
+            alt="setting"
+          />
+          Setting
+        </b-navbar-brand>
+      </router-link>
     </b-navbar>
   </b-container>
 </template>
@@ -36,22 +54,22 @@ export default {
 </script>
 
 <style>
-  .avatar {
-    border-radius: 100%;
-    padding: 1.25rem;
-  }
-  .user {
-    text-align: center;
-    margin: 0;
-  }
-  .navbar {
-    margin: 10px auto;
-  }
-  a.navbar-brand:hover {
-    color: red;
-  }
-  .navbar img {
-    width: 30px;
-    height: 30px;
-  }
+.avatar {
+  border-radius: 100%;
+  padding: 1.25rem;
+}
+.user {
+  text-align: center;
+  margin: 0;
+}
+.navbar {
+  margin: 10px auto;
+}
+a.navbar-brand:hover {
+  color: red;
+}
+.navbar img {
+  width: 30px;
+  height: 30px;
+}
 </style>
